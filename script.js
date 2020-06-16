0f1b2c7113032c0f50903eca9dc87af5a02a1f6c
David Enciso <dpenciso@msn.com>
2020-06-15 20:28:23 -0600

    added vue

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '.instructions',
+    data: {
+        one: "No more than 100,000 characters",
+        two: "Both boxes must contain the same number of characters",
+    }
+})
